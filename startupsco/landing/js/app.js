b729245d45df635a6ba5aecba0d85ffa93568dc9
Noah Halter <noah@startups.co>
2016-08-11 20:42:34 -0400

    t
    t

--- startupsco/landing/js/app.js
+++ startupsco/landing/js/app.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-	$('h1', '#hero').hide().fadeIn(1000);
-	$('p', '#hero').hide().fadeIn(2000);
-});
-
+//$(document).ready(function() {
+//	$('h1', '.hero ').hide().fadeIn(1500);
+//	$('p', '.hero').hide().fadeIn(2000);
+//	});
