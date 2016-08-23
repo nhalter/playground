6426426e87a08ebcaa5766091764a97ed2134f35
Noah Halter <noah@startups.co>
2016-08-23 18:54:31 -0400

    tons
    whatever

---
+++ react/yelp/src/app.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import styles from './styles.module.css'
+import './app.css'
+
+const App = React.createClass({
+  render: function() {
+    return (
+      <div className={styles['container']}>
+        Text text text
+      </div>
+    );
+  }
+});
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(<App />, mountNode);
