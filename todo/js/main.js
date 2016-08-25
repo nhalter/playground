cb391d72caff1ff750cc5d3ed31fade18e0bf48e
Noah Halter <noah@startups.co>
2016-08-25 17:52:26 -0400

    t
    t

--- todo/js/main.js
+++ todo/js/main.js
@@ -1,29 +1,29 @@
 function get_todos() {
-  var todos = new Array;
-  var todos_str = localStorage.getItem('todo');
-  if (todos_str != null) {
-      todos = JSON.parse(todos_str);
-  }
-  return todos;
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str != null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
 }
 
 function add() {
-  var task = document.getElementById('task').value;
+    var task = document.getElementById('task').value;
 
-  var todos = get_todos();
-  todos.push(task);
-  localStorage.setItem('todo', JSON.stringify(todos));
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
 
-  show();
+    show();
 
-  return false;
-  }
+    return false;
+}
 
-  function show() {
+function show() {
     var todos = get_todos();
 
     var html = '<ul>';
-    for(var i=0; i<todos.length; i++) {
+    for (var i = 0; i < todos.length; i++) {
         html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
     };
     html += '</ul>';
@@ -31,12 +31,12 @@ function add() {
     document.getElementById('todos').innerHTML = html;
 
     var buttons = document.getElementsByClassName('remove');
-    for (var i=0; i < buttons.length; i++) {
+    for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', remove);
     };
-  }
+}
 
-  function remove() {
+function remove() {
     var id = this.getAttribute('id');
     var todos = get_todos();
     todos.splice(id, 1);
@@ -45,15 +45,15 @@ function add() {
     show();
 
     return false;
-  }
+}
 
-  document.getElementById("task")
-      .addEventListener("keyup", function(event) {
-      event.preventDefault();
-      if (event.keyCode == 13) {
-          document.getElementById("add").click();
-      }
-  });
+document.getElementById("task")
+    .addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            document.getElementById("add").click();
+        }
+    });
 
 
 document.getElementById('add').addEventListener('click', add);
