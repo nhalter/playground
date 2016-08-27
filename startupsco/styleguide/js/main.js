1bc1e64448ecfca26fa62f0984e7d11272921353
Noah Halter <noah@startups.co>
2016-08-27 15:21:28 -0400

    t
    t

---
+++ startupsco/styleguide/js/main.js
@@ -0,0 +1,87 @@
+var main = function () {
+
+    window.sr = ScrollReveal();
+    sr.reveal('.tldr');
+    sr.reveal('.col');
+
+
+    $('.icon-menu').click(function() {
+
+
+        $('.menu').animate({
+            left: '0px'
+        }, 200);
+
+        $('body').animate({
+            left: '285px'
+        }, 220);
+
+
+
+
+        $(function() {
+
+         var docHeight = $(document).height();
+
+         $("body").append("<div id='overlay'></div>");
+
+         $("#overlay")
+            .height(docHeight)
+            .css({
+               'opacity' : 0.3,
+               'position': 'fixed',
+               'top': 0,
+               'left': 0,
+               'background-color': 'rgba(0,0,0,1)',
+               'width': '100%',
+               'z-index': 5000,
+               'transition' : 'opacity 0.3s ease-in-out'
+             });
+          });
+
+
+
+          $('#overlay').animate({
+              left: '285px'
+          }, 200);
+
+
+
+    $('.icon-close').click(function() {
+        $('.menu').animate({
+            left: '-285px'
+        }, 200);
+
+        $('body').animate({
+            left:'0px'
+        }, 200);
+
+        $('#overlay').animate({
+            left: '-2000px'
+        }, 200);
+
+    });
+
+});
+
+};
+
+$(function() {
+  var a = function() {
+    var b = $(window).scrollTop();
+    var d = $("#scroller-anchor").offset({scroll:false}).top;
+    var c=$(".tabs");
+    if (b>d) {
+      c.css({position:"fixed",top:"0px"})
+    } else {
+      if (b<=d) {
+        c.css({position:"relative",top:""})
+      }
+    }
+  };
+  $(window).scroll(a);a()
+});
+
+
+
+$(document) .ready(main);
